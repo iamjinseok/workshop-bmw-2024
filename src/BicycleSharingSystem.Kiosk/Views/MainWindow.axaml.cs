3a21f4b1dc50c3603c18308e3592313a9303d92a
lukewire <lukewire@dmtek.co.kr>
2024-08-30 12:48:28 +0900

    init avalonia proj

---
+++ src/BicycleSharingSystem.Kiosk/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BicycleSharingSystem.Kiosk.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
